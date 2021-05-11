ae069adaa16edc9cbafbfd9488d039bb8e7d8692
Junhan93 <junhan.3008@gmail.com>
2021-05-11 22:47:08 +0800

    added auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut/>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+
+// withAuthenticator component will scaffold out an entire user authentication flow allowing users to sign up, sign in, reset their password, and confirm sign in for multifactor authentication (MFA)
